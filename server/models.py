bfb052f919f60385c8558be00cbd8bbd356b1f94
cnyaga66 <cnyga66@gmail.com>
2024-07-15 23:22:02 +0300

    changes

--- server/models.py
+++ server/models.py
@@ -1,28 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy_serializer import SerializerMixin
-
-
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
-
+from sqlalchemy_serializer import SerializerMixing
+db = SQLAlchemy()
 class Customer(db.Model):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    Review = Review(back_populates=Review)
-
-    def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
-
-
+    Review = db.relationship('Review',back_populates='customer')
+    items = association_proxy('reviews', 'item')
+    serialize_rules = ('-reviews.customer',)
 class Item(db.Model):
     __tablename__ = 'items'
 
@@ -30,21 +17,14 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
-    Review = Review(back_populates=Review)
-
-    def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
-    
+    review =db.relationship('Review', back_populates='item')
+    serialize_rules = ('-reviews.item',)
 class Review(db.Model):
     __tablename__ = 'reviews'
-    
-    id =  db.Column (db.comment)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), primary_key=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), primary_key=True)
-    review = db.Column(db.String)
-
+    id =  db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(255), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), )
     Customer = Customer(back_populates=Customer)
-    Item = Item(back_populates=Item)
-
-    
-    
+    review = db.Column(db.String)
+    serialize_rules = ('-customer.reviews',-item.reviews)
